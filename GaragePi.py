f3dba26424c3a48c14e985715b50103fadde82fd
Mike <tmdisanto@gmail.com>
2018-10-24 03:06:07 +0000

    More updates to GaragePi.py

--- GaragePi.py
+++ GaragePi.py
@@ -14,19 +14,91 @@ MQTT_CREDENTIALS = 'credentials.txt'
 BROKER_HOST = '192.168.1.120'
 
 #MQTT Topics
-CMD_1 = 'hass/cover1/set'           #Door1 Cmd topic
-CMD_2 = 'hass/cover2/set'           #Door2 Cmd topic
-STATE_1 = 'hass/cover1/state'       #Door1 State topic  
-STATE_2 = 'hass/cover2/state'       #Door2 state topic
-TEMP = 'hass/heat/val'              #Temperature sensor topic
-HUMDITY = 'hass/humidty/val'        #Humidity sensor topic  
-PIR = 'hass/pir/state'              #PIR sensor topic
+CMD_1 = 'hass/cover1/set'                       #Door1 Cmd topic
+CMD_2 = 'hass/cover2/set'                       #Door2 Cmd topic
+STATE_1 = 'hass/cover1/state'                   #Door1 State topic  
+STATE_2 = 'hass/cover2/state'                   #Door2 state topic
+AVAILABILITY_1 = 'hass/cover1/availability'     #Door1 availability topic
+AVAILABILITY_2 = 'hass/cover2/availability'     #Door2 availability topic
+TEMP = 'hass/heat/val'                          #Temperature sensor topic
+HUMDITY = 'hass/humidty/val'                    #Humidity sensor topic  
+PIR = 'hass/pir/state'                          #PIR sensor topic
 
-SUBSCRIPTION_TOPICS = [CMD_1,CMD_2,STATE_1,STATE_2,TEMP,HUMDITY,PIR]
+SUBSCRIPTION_TOPICS = [CMD_1,CMD_2,STATE_1,STATE_2,AVAILABILITY_1,AVAILABILITY_2,TEMP,HUMDITY,PIR]
+
+#MQTT COMMANDS
+CMD_OPEN        = 'OPEN'
+CMD_CLOSE       = 'CLOSE'
+CMD_STOP        = 'STOP'
+STATE_OPEN      = 'open'
+STATE_CLOSED    = 'closed'
+STATE_ONLINE    = 'online'
+STATE_OFFLINE   = 'offline'
 
 #GPIO CONSTANTS
-DHT22_DATA = 27                 #data pin for DHT22 Temp/Humidity sensor
+DOOR1_CTRL      = 17            #output pin controlling door 1
+DOOR2_CTRL      = 22            #output pin controlling door 2
+DOOR1_STATE     = 5             #input pin for determining state of door 1
+DOOR2_STATE     = 6             #input pin for determining state of door 2
+PIR_STATE       = 26            #input pin for determining state of PIR sensor
+DHT22_DATA      = 27            #data pin for DHT22 Temp/Humidity sensor
+LED_RED         = 23            #output pin for Red RGB LED pin
+LED_GREEN       = 24            #output pin for Green RGB LED pin
+LED_BLUE        = 25            #output pin for Blue RGB LED pin
 
+#LED COLOR CONSTANTS
+RED             = (255,0,0)
+GREEN           = (0,255,0)
+BLUE            = (0,0,255)
+YELLOW          = (255,255,0)
+ORANGE          = (255,135,0)
+PURPLE          = (255,0,255)
+CYAN            = (0,255,0)
+LED_OFF         = (0,0,0)
+
+#Other constants
+DOOR1           = 1
+DOOR2           = 2
+DOOR_OPEN       = 0
+DOOR_CLOSED     = 1
+
+
+class RGBLed():
+    """
+    Class to handle controlling an RGB LED
+    """
+    def __init__(self,r,g,b):
+        """
+        Args:
+            r (GPIO.PWM): GPIO.PWM object tied to red LED
+            g (GPIO.PWM): GPIO.PWM object tied to green LED
+            b (GPIO.PWM): GPIO.PWM object tied to blue LED
+        """        
+        self.r = r
+        self.g = g
+        self.b = b
+        
+        #init LED to be off
+        self.r.start(0)
+        self.g.start(0)
+        self.b.start(0)
+
+    def set_color(self,color_code):
+        """
+        Set the color of the LED
+        
+        Args:
+            color_code (tuple): desired color in the form (red,green,blue) and in the range 0-255 per component
+        """
+        #scale the RGB codes to be 0-100
+        R = (color_code[0] / 255) * 100
+        G = (color_code[1] / 255) * 100
+        B = (color_code[2] / 255) * 100
+        #update the duty cycles for each leg of the LED
+        self.r.ChangeDutyCycle(R)
+        self.g.ChangeDutyCycle(G)
+        self.b.ChangeDutyCycle(B)
+        
 def message_callback(callback):
     """
     Decorator function for mqtt message callbacks
@@ -34,9 +106,9 @@ def message_callback(callback):
     @wraps(callback)    #update attribute data for decorator
     def message_handler(client,userdata,message):
         #decode and log the message and return the payload values
-        msg = message.payload.decode("utf-8")
-        logger.debug('Received Message: %s (%s)'%(msg,message.topic))
-        return callback(client,userdata,msg)
+        message.payload = message.payload.decode("utf-8")
+        logger.debug('Received Message: %s (%s)'%(message.payload,message.topic))
+        return callback(client,userdata,message)
     return message_handler
     
 
@@ -98,14 +170,66 @@ def import_credentials():
         except:
             pass 
 
+  
 def setup_gpio():
     """
     Initial setup of the GPIO
     """ 
     logger.info("Setting up GPIO")
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(LED,GPIO.OUT)
-    GPIO.setup(DOOR_INPUT, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    
+    #outputs
+    GPIO.setup(DOOR1_CTRL, GPIO.OUT)
+    GPIO.setup(DOOR2_CTRL, GPIO.OUT)
+    GPIO.setup(LED_RED, GPIO.OUT)
+    GPIO.setup(LED_GREEN, GPIO.OUT)
+    GPIO.setup(LED_BLUE, GPIO.OUT)
+    
+    #inputs
+    GPIO.setup(DOOR1_STATE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(DOOR2_STATE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(PIR_STATE, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(DHT22_DATA, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    
+    
+def toggle_garage_opener(door):
+    """
+    Function to toggle either door 1 or door 2 button
+    
+    Args:
+        door (int) Door 1 (1) Door 2 (2)
+    """
+    if door == DOOR1:
+        pin = DOOR1_CTRL
+    elif door == DOOR2:
+        pin = DOOR2_CTRL
+    else:
+        raise Exception("Invalid door argument (%d)"%door)
+        
+    GPIO.output(pin,GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(pin,GPIO.LOW)
+    
+def get_door_state(door):
+    """
+    Function to get the state of the door
+    
+    Args:
+        door (int) Door 1 (1) Door 2 (2)
+        
+    Returns:
+        state (int): 0 (open) 1 (closed)
+    """
+    if door == DOOR1:
+        pin = DOOR1_CTRL
+    elif door == DOOR2:
+        pin = DOOR2_CTRL
+    else:
+        raise Exception("Invalid door argument (%d)"%door)
+        
+    return GPIO.input(pin)
+    
+    
     
 def read_dht22(client,poll_time=60):
     """
@@ -116,7 +240,7 @@ def read_dht22(client,poll_time=60):
         poll_time (int): Number of seconds between readings
     """
     sensor = Adafruit_DHT.DHT22
-    while 1:
+    while True:
         try:
             humidity, temperature = Adafruit_DHT.read_retry(sensor, pin, DHT22_DATA)
             if temperature is not None and humidity is not None:
@@ -137,11 +261,35 @@ def cleanup():
     GPIO.cleanup()
     client.close()
     
+#MQTT Message Callback functions
+
 @message_callback
 def cover_cmd_callback(client,userdata,message):
-    print('Message: %s | Type: %s'%(message,type(message)))
-    
-    
+    """
+    Message callback for garage cmd topic
+    """
+    if message.topic == CMD_1:
+        door = DOOR1
+    elif message.topic == CMD_2:
+        door = DOOR2
+    else:
+        logger.debug("Invalid topic (%s) for cover_cmd_callback() - Payload: %s"%(message.topic,message.payload))
+        return
+        
+    if message.payload == CMD_OPEN:
+        #open garage door
+        toggle_garage_opener(door)
+
+    elif message.payload == CMD_CLOSE:
+        #close garage door
+        toggle_garage_opener(door)
+        
+    elif message.payload == CMD_STOP:
+        #stop garage door
+    else:
+        #unrecognized command
+        logger.debug("Unrecognized command (%s) on topic: %s"%(message.payload,message.topic))
+ 
     
     
 #### Main ####
@@ -149,5 +297,5 @@ init_logger(LOG_FILE_PATH)      #initialize the logging object
 logger = logging.getLogger('Main')
 
 username,password = import_credentials()
-client = MQTTComms(BROKER_HOST,'GaragePi',SUBSCRIPTION_TOPICS,username,password)
+mqttClient = MQTTComms(BROKER_HOST,'GaragePi',SUBSCRIPTION_TOPICS,username,password)
 
