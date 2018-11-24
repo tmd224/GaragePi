4d761517d0472fec037b2acf52e809dfa0733a54
Mike <tmdisanto@gmail.com>
2018-11-24 18:07:47 +0000

    Gettign the threads module working

--- thread_test.py
+++ thread_test.py
@@ -1,14 +1,14 @@
-
+import time
 from threads import DroneThread
 
-def drone_func():
-    print("Hello Drone Thread")
+def drone_func(name="Noodles"):
+    print("Hello %s Drone Thread"%name)
 
 
 
 if __name__=="__main__":
 
-    d1 = DroneThread(target=drone_func, LoopDelay=2)
+    d1 = DroneThread(target=drone_func, args=("Noody",), kwargs={'LoopDelay':2})
     try:
         d1.start()
         time.sleep(10)
---
+++ threads.py
@@ -0,0 +1,45 @@
+import threading
+import time
+
+
+class StoppableThread(threading.Thread):
+    """Thread class that implements a stop function"""
+
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs=None):
+        self.target = target
+        self.args = args
+        self.kwargs = kwargs                 
+        super(StoppableThread, self).__init__(group=group, target=target, name=name, 
+            args=args, kwargs=kwargs)
+        self._stop_event = threading.Event()
+
+
+        print(self.target)
+    def stop(self):
+        self._stop_event.set()
+
+    def join(self, *args, **kwargs):
+        self.stop()
+        super(StoppableThread, self).join(*args, **kwargs)
+
+
+class DroneThread(StoppableThread):
+    """
+    Stoppable thread that runs a target function in a loop
+    """
+
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs=None):
+        self._loopdelay = kwargs.pop('LoopDelay', 0)
+
+        super(DroneThread, self).__init__(group=group, target=target, name=name,
+                 args=args, kwargs=kwargs)
+        
+    def run(self):
+        print("Calling run method")
+        if self.target:
+            print("Target is not None")
+            while not self._stop_event.is_set():
+                self.target(*self.args, **self.kwargs)   # run the target function
+                time.sleep(self._loopdelay)
